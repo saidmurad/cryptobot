c7e2c269411a5e3886f464d1ffd62aa428d8483c
Kannan J <kannan.jayaprakasam@gmail.com>
2022-01-31 01:02:10 +0530

    Placing Spot order for buy.

--- src/main/java/com/binance/bot/trading/SupportedSymbolsInfo.java
+++ src/main/java/com/binance/bot/trading/SupportedSymbolsInfo.java
@@ -3,6 +3,8 @@ package com.binance.bot.trading;
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.OrderType;
+import com.binance.api.client.domain.general.FilterType;
+import com.binance.api.client.domain.general.SymbolFilter;
 import com.binance.api.client.domain.general.SymbolStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,7 @@ public class SupportedSymbolsInfo {
 
   private BinanceApiRestClient binanceApiRestClient;
   private Map<String, List<OrderType>> symbolInfoMap = new HashMap<>();
+  private Map<String, Integer> lotSizeMap = new HashMap<>();
   private final Logger logger = LoggerFactory.getLogger(getClass());
   private Long lastFetched = null;
 
@@ -39,5 +42,27 @@ public class SupportedSymbolsInfo {
     });
     logger.info(String.format("Returning symbol map with %d symbols.", symbolInfoMap.size()));
     return symbolInfoMap;
+  }
+
+  private Integer getTickSizeAsNum(String tickSizeStr) {
+    Double tickSize = Double.parseDouble(tickSizeStr);
+    int count = 0;
+    while (tickSize < 1.0) {
+      tickSize *= 10;
+      count++;
+    }
+    return count;
+  }
+
+  public Integer getLotSize(String symbol) {
+    if (lotSizeMap.isEmpty()) {
+      binanceApiRestClient.getExchangeInfo().getSymbols().parallelStream().forEach(symbolInfo -> {
+        String stepSize = symbolInfo.getSymbolFilter(FilterType.LOT_SIZE).getStepSize();
+        if (stepSize != null) {
+          lotSizeMap.put(symbolInfo.getSymbol(), getTickSizeAsNum(stepSize));
+        }
+      });
+    }
+    return lotSizeMap.get(symbol);
   }
 }

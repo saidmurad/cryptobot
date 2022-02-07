8dbc18600c491acbad7c9735990c137883b60956
Kannan J <kannan.jayaprakasam@gmail.com>
2022-02-07 09:09:04 +0530

    Incomplete changes for adding attempt count.

---
+++ src/main/java/com/binance/bot/altfins/AltfinPatternsReaderBookkeeping.java
@@ -0,0 +1,10 @@
+package com.binance.bot.altfins;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class AltfinPatternsReaderBookkeeping {
+  Date[] earliestChartPatternTimesInThisRun = new Date[4];
+}

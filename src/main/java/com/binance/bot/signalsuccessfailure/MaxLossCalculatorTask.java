b7e40438b28809c6f805afe32a93d086a821dbd7
Kannan J <kannan.jayaprakasam@gmail.com>
2022-02-08 19:37:41 +0530

    Add logging before inserting into db.

--- src/main/java/com/binance/bot/signalsuccessfailure/MaxLossCalculatorTask.java
+++ src/main/java/com/binance/bot/signalsuccessfailure/MaxLossCalculatorTask.java
@@ -5,15 +5,23 @@ import com.binance.bot.tradesignals.ChartPatternSignal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
 import java.util.List;
 
 public class MaxLossCalculatorTask {
   @Autowired
   private ChartPatternSignalDaoImpl dao;
 
-  @Scheduled(fixedDelay = 60000)
+  //@Scheduled(fixedDelay = 600000)
   public void perform() {
-    List<ChartPatternSignal> chartPatternSignals = dao.getAllChartPatterns();
+    List<ChartPatternSignal> chartPatternSignals = dao.getAllChartPatternsNeedingMaxLossCalculated();
+    for (ChartPatternSignal chartPatternSignal: chartPatternSignals) {
+      Date signalTime = chartPatternSignal.timeOfSignal();
+      Date signalTargetTime = chartPatternSignal.priceTargetTime();
+      Date currWindowStart = signalTime;
+      while (currWindowStart.before(signalTargetTime)) {
 
+      }
+    }
   }
 }

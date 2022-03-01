e09801b880c6fb959c8e5522a63026859d162856
Kannan J <kannan.jayaprakasam@gmail.com>
2022-03-01 10:56:59 +0530

    Separate logging for MaxLossCalculatorTask.

--- src/main/java/com/binance/bot/signalsuccessfailure/MaxLossCalculatorTask.java
+++ src/main/java/com/binance/bot/signalsuccessfailure/MaxLossCalculatorTask.java
@@ -60,8 +60,8 @@ public class MaxLossCalculatorTask {
       Long fromId = null;
       long beginTime = System.currentTimeMillis();
       while (!isDone) {
-        logger.info(String.format("Calling getAggrTrades with fromId %d.",
-            fromId == null? null : Long.toString(fromId)));
+        logger.info(String.format("Calling getAggrTrades with fromId %s.",
+            fromId == null? "null" : Long.toString(fromId)));
         List<AggTrade> aggTrades = binanceApiRestClient.getAggTrades(
             chartPatternSignal.coinPair(), fromId == null? null : Long.toString(fromId), 1000,
             firstIteration ? signalTime : null, firstIteration? getToTime(signalTime, chartPatternSignal) : null);
